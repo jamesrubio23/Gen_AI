0f9c5f13a0df7cfc15915036ac373f3d182f58e4
jamesrubio23 <jamesrubiojames@gmail.com>
2024-10-01 17:47:49 +0200

    gunicorn uvicorn[standard]  added

--- to_do_list_app/002-nextjs-frontend/todo-app/next.config.js
+++ to_do_list_app/002-nextjs-frontend/todo-app/next.config.js
@@ -1,6 +1,22 @@
-/** @type {import('next').NextConfig} */
+
+/* @type {import('next').NextConfig} 
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = nextConfig*/
+
+const path = require('path');
+ 
+const nextConfig = {
+  reactStrictMode: true,
+ 
+  // Extiende la configuración de Webpack
+  webpack: (config) => {
+    // Configura un alias '@' que apunta a la raíz del directorio del proyecto
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+}
+ 
+module.exports = nextConfig;
